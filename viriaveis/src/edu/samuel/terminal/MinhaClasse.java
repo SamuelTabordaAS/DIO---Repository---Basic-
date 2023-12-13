9568621e798fbe9ffe449ba526c9896e340416d1
Taborda <samueltabordaas@gmail.com>
2023-12-13 12:40:32 -0300

    mais conteudos

---
+++ viriaveis/src/edu/samuel/terminal/MinhaClasse.java
@@ -0,0 +1,10 @@
+package edu.samuel.terminal; 
+
+public class MinhaClasse{ 
+
+
+    public static void main(String[] args) {
+        
+        System.out.println("Fui executado pelo TERMINAL ! ");
+    }
+}
